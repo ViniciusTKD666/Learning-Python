843f21d5230392a8d091795949d455cea56978f0
Vinicius Sousa <viniciusblue10@gmail.com>
2023-03-22 09:02:01 -0300

    Default

---
+++ Exercicios/Teste de formatacao.py
@@ -0,0 +1,14 @@
+#interpolation
+nome = "Vinicius"
+sobrenome = "De Sousa"
+
+print("O nome é '{}', e o sobrenome é '{}'!!".format(nome, sobrenome))
+
+#value formatting
+value1 = 4.5
+value2 = 1245
+value3 = 55.68
+
+print("Os valores dos objetos são ${:.2f}, ${:.2f} e ${:.2f}".format(value1, value2, value3))
+
+
