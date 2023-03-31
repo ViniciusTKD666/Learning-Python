4ce639cb67671410d74a5c17ffb3cc72a14bfdff
Vinicius Sousa <viniciusblue10@gmail.com>
2023-03-31 09:24:52 -0300

    Default

---
+++ Exercicios/Seletor de jogos.py
@@ -0,0 +1,13 @@
+print("**************************************")
+print("**Escolha o jogo que você quer jogar**")
+print("**************************************")
+
+print("[1] Jogo da forca | [2] Jogo de advinhacao")
+
+seletor = int(input("Qual é o jogo?: "))
+
+if(seletor == 1):
+    print("escolheu o jogo 1")
+
+elif(seletor == 2):
+    print("escolheu o jogo 2")
